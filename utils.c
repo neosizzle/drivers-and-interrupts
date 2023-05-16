5711c8e837f68bef0003b52d4f1793573b558928
jng <junhanngd1115@gmail.com>
2023-05-16 14:40:53 +0800

    feat: color removal

--- utils.c
+++ utils.c
@@ -3,10 +3,10 @@
 
 void ft_log(char *str)
 {
-	printk(KERN_INFO YEL "[42KB] %s" RESET, str);
+	printk(KERN_INFO "[42KB] %s", str);
 }
 
 void ft_warn(char *str)
 {
-	printk(KERN_WARNING YEL "[42KB] WARN - %s" RESET, str);
+	printk(KERN_WARNING "[42KB] WARN - %s", str);
 }
