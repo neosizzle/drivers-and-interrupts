6f5551f28158073ecc222962cec8c7fad88ac12a
jng <junhanngd1115@gmail.com>
2023-05-26 20:51:46 +0800

    feat: tmpfile boiler

--- tmpfile.c
+++ tmpfile.c
@@ -2,12 +2,12 @@
  * tmpfile.c - /tmp/42module_keyboard file configration
 */
 #include "42kb.h"
-#include <linux/syscalls.h>
+#include <linux/fs.h>
 
 int ft_create_tmpfile(void)
 {
-	fd = sys_open("/tmp/lol", O_RDONLY | O_CREAT, 0);
-	if (fd >= 0) {
+	struct file* infile = filp_open("/tmp/lol", O_RDONLY | O_CREAT, S_IRWXU);
+	if (infile) {
 		printk("opened file\n");
 		sys_close(fd);
 	}
