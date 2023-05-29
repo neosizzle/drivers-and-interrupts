c97b277503d0123279ed75a3c27df37724070c32
jng <junhanngd1115@gmail.com>
2023-05-29 10:11:31 +0800

    feat: tmpfile unlink

--- tmpfile.c
+++ tmpfile.c
@@ -26,14 +26,19 @@ int ft_create_tmpfile(void)
 // TODO unlink tmpfile
 void ft_destroy_tmpfile(void)
 {
-	 struct inode *parent_inode;
+	struct inode *parent_inode;
+	char *parent_name;
 
 	if (!tmpfile) return;
 	parent_inode = tmpfile->f_path.dentry->d_parent->d_inode;
+	parent_name = tmpfile->f_path.dentry->d_parent.d_name.name;
 	// if (file_count(tmpfile) > 0)
+	printk("parent got\n");
 	filp_close(tmpfile, NULL);
 	if (!parent_inode) return;
 	inode_lock(parent_inode);
+	printk("unlinking parent %s wtih destination %s\n",parent_name ,tmpfile->f_path.dentry.d_name.name);
 	vfs_unlink(NULL, parent_inode, tmpfile->f_path.dentry, NULL);
 	inode_unlock(parent_inode);
+	printk("vfs unlinked\n");
 }
