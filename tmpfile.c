31be691c04ebc7daa57af36ad1d0b10679ad3149
jng <junhanngd1115@gmail.com>
2023-05-29 10:51:08 +0800

    feat: tmpfile unlink

--- tmpfile.c
+++ tmpfile.c
@@ -37,8 +37,9 @@ void ft_destroy_tmpfile(void)
 	printk("unlinking parent %s wtih destination %s\n",parent_name ,tmpfile->f_path.dentry->d_name.name);
 
 	inode_lock(parent_inode);
-	vfs_unlink(NULL, parent_inode, tmpfile->f_path.dentry, NULL);
+	struct dentry *tmpdentry = tmpfile->f_path.dentry;
 	filp_close(tmpfile, NULL); // see if put here works?
+	vfs_unlink(NULL, parent_inode, tmpdentry, NULL);
 	inode_unlock(parent_inode);
 	printk("vfs unlinked\n");
 }
