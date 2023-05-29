c38e24ec3433afeebd553ca3c1eeb7843682f6e7
jng <junhanngd1115@gmail.com>
2023-05-29 10:26:03 +0800

    feat: tmpfile unlink

--- tmpfile.c
+++ tmpfile.c
@@ -29,12 +29,12 @@ void ft_destroy_tmpfile(void)
 	struct inode *parent_inode;
 
 	if (!tmpfile) return;
-	parent_inode = tmpfile->f_path.dentry->d_parent->d_inode;
+	// parent_inode = tmpfile->f_path.dentry->d_parent->d_inode;
 	// char *parent_name = tmpfile->f_path.dentry->d_parent->d_name.name;
 	// if (file_count(tmpfile) > 0)
-	printk("parent got\n");
+	// printk("parent got\n");
 	filp_close(tmpfile, NULL);
-	if (!parent_inode) return;
+	// if (!parent_inode) return;
 	printk("unlinking parent %s wtih destination %s\n","wut" ,tmpfile->f_path.dentry->d_name.name);
 
 	// inode_lock(parent_inode);
