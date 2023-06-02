d486077c3b84d083a8c9f2950a895863a76aa4e0
jng <junhanngd1115@gmail.com>
2023-06-02 16:30:19 +0800

    feat: devfile read

--- device.c
+++ device.c
@@ -44,8 +44,9 @@ static ssize_t ft_module_keyboard_read(struct file *file, char *buff, size_t, lo
 
 		temp_str = event_to_str(*entry);
 		printk("tempstr.. %s|", output_str);
-		output_str = strcat(output_str, temp_str);
+		strcat(temp_str, output_str);
 		// kfree(temp_str);
+
 		head_ptr = head_ptr->next;
 	} while (head_ptr != &(g_driver->events_head->list));
 
