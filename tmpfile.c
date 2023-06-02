8c79fa588a1e0d3900db861d631583f1fdcea944
jng <junhanngd1115@gmail.com>
2023-06-02 16:49:26 +0800

    feat: devfile read

--- tmpfile.c
+++ tmpfile.c
@@ -38,37 +38,6 @@ int ft_create_tmpfile(void)
 	}
 }
 
-static char    *ft_itoa(time64_t nbr)
-{
-	int		len;
-	long	n;
-	char	*str;
-
-	// if (nbr == -2147483648)
-	// 	return ("-2147483648");
-	len = 0;
-	n_tmp = nbr;
-	while (n_tmp)
-	{
-		n_tmp /= 10
-		len += 1;
-	}
-	if (nbr < 0)
-	{
-		len += 1;
-		nbr *= -1;
-	}
-	if (!(str = (char *)kmalloc(sizeof(char) * len + 1)))
-		return (NULL);
-	str[len] ='\0';
-	while (nbr)
-	{
-		str[len] = (n % 10) + '0';
-		nbr /= 10;
-	}
-	return (str);
-}
-
 // void ft_log_tmpfile(void)
 // {
 // 	time64_t time;
