295c778ebad836bf8ed67c0c4ba8ffb6bbb0a6c1
jng <junhanngd1115@gmail.com>
2023-06-03 15:44:39 +0800

    feat: tmpfile write ascii instead

--- utils.c
+++ utils.c
@@ -166,7 +166,7 @@ event_struct *ft_generate_event(queue_data q_data, int scancode)
 	res->is_pressed = is_pressed;
 	res->name = key.name ? (is_upper > 0? key.caps_name : key.name) : "unknown";
 	res->time = ktime_get_seconds();
-	res->ascii_value = !is_upper ? key.caps_ascii : key.ascii;
+	res->ascii_value = is_upper > 0 ? key.caps_ascii : key.ascii;
 	
 	// init list head
 	INIT_LIST_HEAD(&(res->list));
